9438881c61069c186bdafac5e0706e8ad3848bb5
Rails udemy Course <jeboyzz@yahoo.com>
2017-07-03 07:23:22 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  #section7 lecture 144---
+  has_many :article_categories #because the many side
+  has_many :categories, through: :article_categories
+  #section7 lecture 144---
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
+  #section7 lecture 144---
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  #section7 lecture 144---
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
   
